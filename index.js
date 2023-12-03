b64cd26597a9d4da602c68925dd707c2778d63de
Marco Pavan <marcopavan.mp@gmail.com>
2023-12-03 17:26:41 +0100

    Clean + rest + eslint + small

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express')
+const express = require('express');
 const https = require('https');
 const fs = require('fs');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-/*app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})*/
+
 // LISTENING PORT
 const PORT = process.env.PORT || 8088;
 https.createServer({
@@ -19,5 +17,3 @@ https.createServer({
 }, app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT} over HTTPS.`);
 });
-
-//module.exports = app
