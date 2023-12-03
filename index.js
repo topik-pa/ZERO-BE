0d0c32d13eafbe70f892514401c341a810387b38
Marco Pavan <marcopavan.mp@gmail.com>
2023-12-03 17:18:33 +0100

    Added HTTPS

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const https = require('https');
+const fs = require('fs');
+
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+})*/
+// LISTENING PORT
+const PORT = process.env.PORT || 8088;
+https.createServer({
+  key: fs.readFileSync('./cert/server.key'),
+  cert: fs.readFileSync('./cert/server.cert')
+}, app).listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} over HTTPS.`);
+});
+
+//module.exports = app
